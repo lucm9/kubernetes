cd377ff7ff6ddee4981491e8206569874782a074
lucm9 <lucmally@yahoo.com>
2024-08-21 04:43:19 +0000

    k8

--- back/back.py
+++ back/back.py
@@ -1,58 +1,47 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from random import randint
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text # type: ignore
-from sqlalchemy import exc
+from sqlalchemy import text, exc
 import json
-from random import randint
-from flask import request
-
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI'] #
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = 'changethis'
+
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the Quotes API!"}), 200
 
 @app.route('/api/v1/get-quote', methods=['GET'])
 def get_quote():
-    # Get the number of quotes saved in out DB
     length_query = text('SELECT COUNT(id) FROM quotes')
     length_result = db.engine.execute(length_query)
     length = length_result.fetchone()[0]
-    # Return a random integer random_id where: 1 < random_id < length
     random_id = randint(1, int(length))
-    # Selecting a random quote using a random quote id
-    sql = text('select quote from quotes where id=%s' % (random_id) )
-    result = db.engine.execute(sql)
-    # Returning the quote
+    sql = text('SELECT quote FROM quotes WHERE id=:id')
+    result = db.engine.execute(sql, {'id': random_id})
     random_quote = result.first()[0]
-    return json.dumps({'random_quote':random_quote}), 200, {'ContentType':'application/json'}
+    return jsonify({'random_quote': random_quote}), 200
 
 @app.route('/api/v1/set-quote', methods=['POST'])
 def set_quote():
-    # Reading the POST JSON data using the key "quote"
-    quote = request.json['quote']
-    # Inserting the quote in the database
-    sql = text('INSERT INTO quotes (quote) VALUES ("%s");' % quote)
-    # This will return a success message if the quote is inserted otherwise it wil return a 500 server error
+    quote = request.json.get('quote')
+    sql = text('INSERT INTO quotes (quote) VALUES (:quote)')
     try:
-        result = db.engine.execute(sql)
+        db.engine.execute(sql, {'quote': quote})
     except exc.IntegrityError:
-        return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-
+        return jsonify({'success': False}), 500
+    return jsonify({'success': True}), 200
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
     try:
-        db.session.query("1").from_statement("SELECT 1").all()
-        return json.dumps({'up':True}), 200, {'ContentType':'application/json'}
+        db.session.query("1").from_statement(text("SELECT 1")).all()
+        return jsonify({'up': True}), 200
     except:
-        return json.dumps({'up':False}), 500, {'ContentType':'application/json'}
+        return jsonify({'up': False}), 500
 
-
-# This API will run on port 3000 on host 0.0.0.0.
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
